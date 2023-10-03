3b8ed4abdd0d4211c7dc65878496d33b4e97c27d
ukit3ro <Crasher985@yandex.ru>
2023-10-03 17:28:51 +0500

    assssd

--- №12_bases/practice.py
+++ №12_bases/practice.py
@@ -109,5 +109,17 @@ while '>2' or '>3' or '>5' in s:
         s = s.replace('>3', '3>', 1)
     if '>5' in s:
         s = s.replace('>5', '2>', 1)
-    
-        
+print(s.count('3')*3 + s.count('2')*2 + s.count('5')*2)
+
+
+#задача №18
+for n in range(4, 2000):
+    s = '5' + '2' * n
+    while '52' or '2222' or '3322' in s:
+        if '52' in s:
+            s = s.replace('52', '33', 1)
+        if '2222' in s:
+            s = s.replace('2222', '5', 1)
+        if '3322' in s:
+            s = s.replace('3322', '25', 1)
+    summ = s.count('2')*2 + s.count('3')*3 + s.count('5')*5
